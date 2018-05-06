4df7aa0a9c997dde1fed28c0180c72bf5470ec26
Raphael Muenster <raphael.muenster@gmail.com>
2018-05-06 17:51:09 +0200

    Added files for static server

---
+++ http_server_static/http_server_static.js
@@ -0,0 +1,22 @@
+var fs = require('fs');
+var http = require('http');
+var url = require('url');
+
+var ROOT_DIR = '/html';
+
+//========================================================================
+// Create the http server with a callback function that
+// takes the request and response objects
+//========================================================================
+http.createServer(function(req, res) {
+  var urlObj = url.parse(req.url, true, false);
+  fs.readFile( ROOT_DIR + urlObj.pathname, function(err, data){
+    if (err) {
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+      return; 
+    }
+    res.writeHead(200);
+    res.end(data);
+  });
+}).listen(8080);
